31b59c69c28a9920686e9ffdf59fe1e83fb26e54
Jewoo <jeus0630@gmail.com>
2022-02-19 21:27:10 +0900

    Update: : MapBox 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Map from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
+  useEffect(()=>{
+    return () => {
+        
+    }
+  },[]);
   return (
     <div className="App">
-      
+      <Map
+      initialViewState={{
+        longitude: 127.0403481,
+        latitude: 37.7269078,
+        zoom: 14
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+    />
     </div>
   );
-}
+} 
 
 export default App;
