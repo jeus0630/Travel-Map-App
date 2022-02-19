2770f54c4cc0c9d56863a940fe0dea8909e8a790
Jewoo <jeus0630@gmail.com>
2022-02-20 01:20:52 +0900

    Docs : App.scss, reset.css
    UseState -> UseReducer
    Add : Scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,61 +1,129 @@
-import React, { useEffect, useState } from 'react';
-import Map, {Marker, Popup} from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import {Room} from '@material-ui/icons';
+import React, { useEffect, useReducer } from "react";
+import Map, { Marker, Popup } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { Room, Star } from "@material-ui/icons";
+import "./App.scss";
+
+type stateType = {
+  latitude: number;
+  longitude: number;
+  showPopup: boolean;
+};
+
+type ActionType =
+  | { type: "latitude"; payload: number }
+  | { type: "longitude"; payload: number }
+  | { type: "popup"; payload: boolean };
+
+type reducerType = (state: stateType, action: ActionType) => stateType;
+
+const reducer: reducerType = (state: stateType, action: ActionType) => {
+  switch (action.type) {
+    case "latitude":
+      return {
+        ...state,
+        latitude: action.payload,
+      };
+    case "longitude":
+      return {
+        ...state,
+        longitude: action.payload,
+      };
+    case "popup":
+      return {
+        ...state,
+        showPopup: action.payload,
+      };
+    default:
+      return initialState;
+  }
+};
+
+const initialState = {
+  latitude: 0,
+  longitude: 0,
+  showPopup: true,
+};
 
 function App() {
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
-  const [showPopup, setShowPopup] = React.useState(true);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { latitude, longitude, showPopup } = state;
+  useEffect(() => {
+    const showPosition = (position: {
+      coords: { latitude: number; longitude: number };
+    }): void => {
+      dispatch({ type: "latitude", payload: position.coords.latitude });
+      dispatch({ type: "longitude", payload: position.coords.longitude });
+    };
 
-  useEffect(()=>{
-    const showPosition = (position: { coords: { latitude: number; longitude: number; }; }): void => {
-      
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    }
-
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPosition);
-    }else{
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
       //허용 안할시 기본값 판교
-      setLatitude(37.3897);
-      setLongitude(127.1017);
+      dispatch({ type: "latitude", payload: 37.3897 });
+      dispatch({ type: "longitude", payload: 127.1017 });
     }
 
-    return () => {
-
-    }
-  },[]);
+    return () => {};
+  }, []);
 
   return (
     <div className="App">
-      {
-        latitude && longitude && 
+      {latitude && longitude && (
         <Map
-        initialViewState={{
-          longitude,
-          latitude,
-          zoom: 16
-        }}
-        style={{width: "100vw", height: "100vh"}}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-      >
-        <Marker longitude={longitude} latitude={latitude} anchor="bottom" >
-          <Room style={{color: "slateblue"}}></Room>  
-          <strong style={{color: "slateblue"}}>I am HERE</strong>
-        </Marker>
-        {showPopup && (
-      <Popup longitude={longitude} latitude={latitude}
-        anchor="bottom"
-        onClose={() => setShowPopup(false)}>
-        You are here
-      </Popup>)}
+          initialViewState={{
+            longitude,
+            latitude,
+            zoom: 16,
+          }}
+          style={{ width: "100vw", height: "100vh" }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        >
+          <Marker longitude={longitude} latitude={latitude} anchor="bottom">
+            <Room style={{ color: "slateblue" }}></Room>
+            <strong style={{ color: "slateblue" }}>I am HERE</strong>
+          </Marker>
+          {showPopup && (
+            <Popup
+              longitude={longitude}
+              latitude={latitude}
+              anchor="left"
+              onClose={() => dispatch({ type: "popup", payload: false })}
+            >
+              <ul className="card">
+                <li>
+                  <em>Place</em>
+                  <p className="place">My house</p>
+                </li>
+                <li>
+                  <em>Review</em>
+                  <p>Beautiful Place I like it</p>
+                </li>
+                <li>
+                  <em>Rating</em>
+                  <div className="stars">
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                  </div>
+                </li>
+                <li>
+                  <em>Information</em>
+                  <span className="username">
+                    Created by <b>Jewoo</b>
+                  </span>
+                  <span className="date">1 hour ago</span>
+                </li>
+              </ul>
+            </Popup>
+          )}
         </Map>
-      }
+      )}
     </div>
   );
-} 
+}
 
 export default App;
