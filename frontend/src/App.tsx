a0be1633e1347d367fd6ec22cbe0d5d69e36928b
Jewoo <jeus0630@designfever.com>
2022-02-21 00:42:54 +0900

    Fix : App.tsx
    FetchData bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -50,8 +50,7 @@ const reducer: reducerType = (state: stateType, action: ActionType) => {
     case "pins":
       return {
         ...state,
-        ...state.pins,
-        ...action.payload
+        pins: action.payload
       };
     default:
       return initialState;
@@ -79,21 +78,6 @@ function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { latitude, longitude, showPopup } = state;
   useEffect(() => {
-    const showPosition = (position: {
-      coords: { latitude: number; longitude: number };
-    }): void => {
-      dispatch({ type: "latitude", payload: position.coords.latitude });
-      dispatch({ type: "longitude", payload: position.coords.longitude });
-    };
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      //허용 안할시 기본값 판교
-      dispatch({ type: "latitude", payload: 37.3897 });
-      dispatch({ type: "longitude", payload: 127.1017 });
-    }
-
     const pins = async () => {
       try {
         const res = await fetch('/pins');
@@ -104,69 +88,81 @@ function App() {
       }
     }
 
-    pins();
+    const showPosition = (position: {
+      coords: { latitude: number; longitude: number };
+    }): void => {
+      dispatch({ type: "latitude", payload: position.coords.latitude });
+      dispatch({ type: "longitude", payload: position.coords.longitude });
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+      pins();
+    } else {
+      //허용 안할시 기본값 판교
+      dispatch({ type: "latitude", payload: 37.3897 });
+      dispatch({ type: "longitude", payload: 127.1017 });
+      pins();
+    }
 
     return () => { };
   }, []);
 
-
-
   if (latitude && longitude) {
+
     return (
       <div className="App">
-        {latitude && longitude && (
-          <Map
-            initialViewState={{
-              longitude,
-              latitude,
-              zoom: 16,
-            }}
-            style={{ width: "100vw", height: "100vh" }}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-            mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-          >
-            <Marker longitude={longitude} latitude={latitude} anchor="bottom">
-              <Room style={{ color: "slateblue" }}></Room>
-              <strong style={{ color: "slateblue" }}>I am HERE</strong>
-            </Marker>
-            {showPopup && (
-              <Popup
-                longitude={longitude}
-                latitude={latitude}
-                anchor="left"
-                onClose={() => dispatch({ type: "popup", payload: false })}
-              >
-                <ul className="card">
-                  <li>
-                    <em>Place</em>
-                    <p className="place">My house</p>
-                  </li>
-                  <li>
-                    <em>Review</em>
-                    <p>Beautiful Place I like it</p>
-                  </li>
-                  <li>
-                    <em>Rating</em>
-                    <div className="stars">
-                      <Star></Star>
-                      <Star></Star>
-                      <Star></Star>
-                      <Star></Star>
-                      <Star></Star>
-                    </div>
-                  </li>
-                  <li>
-                    <em>Information</em>
-                    <span className="username">
-                      Created by <b>Jewoo</b>
-                    </span>
-                    <span className="date">1 hour ago</span>
-                  </li>
-                </ul>
-              </Popup>
-            )}
-          </Map>
-        )}
+        <Map
+          initialViewState={{
+            longitude,
+            latitude,
+            zoom: 16,
+          }}
+          style={{ width: "100vw", height: "100vh" }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        >
+          <Marker longitude={longitude} latitude={latitude} anchor="bottom">
+            <Room style={{ color: "slateblue" }}></Room>
+            <strong style={{ color: "slateblue" }}>I am HERE</strong>
+          </Marker>
+          {showPopup && (
+            <Popup
+              longitude={longitude}
+              latitude={latitude}
+              anchor="left"
+              onClose={() => dispatch({ type: "popup", payload: false })}
+            >
+              <ul className="card">
+                <li>
+                  <em>Place</em>
+                  <p className="place">My house</p>
+                </li>
+                <li>
+                  <em>Review</em>
+                  <p>Beautiful Place I like it</p>
+                </li>
+                <li>
+                  <em>Rating</em>
+                  <div className="stars">
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                  </div>
+                </li>
+                <li>
+                  <em>Information</em>
+                  <span className="username">
+                    Created by <b>Jewoo</b>
+                  </span>
+                  <span className="date">1 hour ago</span>
+                </li>
+              </ul>
+            </Popup>
+          )}
+        </Map>
       </div>
     );
   }
